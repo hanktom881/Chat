d9282aeb51e7733ef2a40aab213cc6d6177f2cce
Hank Tom <hanktom@gmail.com>
2015-08-25 11:56:40 +0800

    可輸入帳號名稱, 再註冊取得rid

--- app/src/main/java/com/tom/chat/MainActivity.java
+++ app/src/main/java/com/tom/chat/MainActivity.java
@@ -3,8 +3,11 @@ package com.tom.chat;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -24,23 +27,25 @@ public class MainActivity extends Activity {
     GoogleCloudMessaging gcm ;
     public static final String USERID = "tom";
     Registration regService;
+    private EditText edUserid;
+    String rid = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new GCMRegistrationAsyncTask().execute();
+        edUserid = (EditText)findViewById(R.id.userid);
     }
 
     class GCMRegistrationAsyncTask extends AsyncTask<Void, Void, String>{
         private static final String SENDER_ID = "642623149983";
-        String rid = null;
+
         @Override
         protected String doInBackground(Void... params) {
             //向GCM雲端註冊 並取得Registration Id
             if (gcm==null){
                 gcm = GoogleCloudMessaging.getInstance(getApplicationContext());
             }
-            if (regService==null){
+            /*if (regService==null){
                 Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
                         new AndroidJsonFactory(), null)
                         .setRootUrl("https://level-bond-102308.appspot.com/_ah/api/")
@@ -51,18 +56,18 @@ public class MainActivity extends Activity {
                             }
                         });
                 regService = builder.build();
-            }
+            }*/
 
             try {
                 rid = gcm.register(SENDER_ID);
                 //送到GAE(backend)
-                regService.register(rid).execute();
+//                regService.register(rid).execute();
                 //將rid與userid 送到後端儲存
-                /*URL url = new URL("http://140.137.215.10:8080/atm/r?rid="+rid+"&uid="+USERID);
+                URL url = new URL("http://140.137.215.10:8080/chatd/r?rid="+rid+"&uid="+edUserid.getText().toString());
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 InputStream is = conn.getInputStream();
                 int data = is.read();
-                is.close();*/
+                is.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -71,10 +76,15 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(String s) {
+            Log.d("RID", s);
             if (s!=null){
                 Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    public void register(View v){
+        new GCMRegistrationAsyncTask().execute();
     }
 
     @Override
